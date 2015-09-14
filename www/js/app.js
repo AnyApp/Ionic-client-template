4d998a3caeed732b8576278cc027472be5592df5
Or Hemi <hemiorr@gmail.com>
2015-09-14 16:09:28 +0300

    Add form validation. Finish signup page

--- www/js/app.js
+++ www/js/app.js
@@ -3,65 +3,74 @@ var app = angular.module('app', [
     'ionic',
     'ionic.service.core',
     'pascalprecht.translate',
+    'ngMessages',
 
     'appControllers',
     'appServices',
     'appDirectives'
 ]);
 
-app.run(function($rootScope, $ionicPlatform) {
+app.run(function ($rootScope, $ionicPlatform) {
 
     $rootScope.user = false;
 
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleLightContent();
+        }
+    });
 });
 
-app.config(function($stateProvider, $urlRouterProvider, $translateProvider) {
+app.config(function ($stateProvider, $urlRouterProvider, $translateProvider) {
 
-  $stateProvider
-  // setup an abstract state for the tabs directive
-  // Each tab has its own nav history stack:
-  .state('login', {
-          url: '/login',
-          controller: 'LoginCtrl',
-          templateUrl: 'templates/login.html'
-  })
-  .state('signup', {
-          url: '/signup',
-          controller: 'SignupCtrl',
-          templateUrl: 'templates/signup.html'
+    $stateProvider
+        // setup an abstract state for the tabs directive
+        // Each tab has its own nav history stack:
+        .state('login', {
+            url: '/login',
+            controller: 'LoginCtrl',
+            templateUrl: 'templates/login.html'
+        })
+        .state('signup', {
+            url: '/signup',
+            controller: 'SignupCtrl',
+            templateUrl: 'templates/signup.html'
+        })
+        .state('', {
+            url: '/signup',
+            controller: 'SignupCtrl',
+            templateUrl: 'templates/signup.html'
+        })
+    ;
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+
+    $translateProvider.translations('he_IL', {
+        'LOGIN_TITLE': 'התחברות',
+        'SIGNUP_TITLE': 'הרשמה',
+        'USERNAME': 'שם משתמש',
+        'PASSWORD': 'סיסמא',
+        'LOGIN': 'התחבר',
+        'SIGNUP': 'הירשם',
+        'LOGIN_FACEBOOK': 'התחבר באמצעות פייסבוק',
+        'LOGIN_GOOGLE': 'התחבר באמצעות גוגל',
+        'SIGNUP_MSG': 'הזן מספר פרטים והתחל לשלם למוניות מהפלאפון',
+        'MAIL': 'כתובת מייל',
+        'FULLNAME': 'שם מלא',
+        'FORM_MSG.REQUIRED': 'שדה חובה',
+        'FORM_MSG.MINLENGTH': 'חייב להכיל לפחות {{value}} תווים',
+        'FORM_MSG.EMAIL': 'כתובת אימייל לא חוקית'
     });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/login');
 
-  $translateProvider.translations('he_IL', {
-      'LOGIN_TITLE': 'התחברות',
-      'SIGNUP_TITLE': 'הרשמה',
-      'USERNAME': 'שם משתמש',
-      'PASSWORD': 'סיסמא',
-      'LOGIN': 'התחבר',
-      'SIGNUP': 'הירשם',
-      'LOGIN_FACEBOOK': 'התחבר באמצעות פייסבוק',
-      'LOGIN_GOOGLE': 'התחבר באמצעות גוגל',
-      'SIGNUP_MSG': 'הזן מספר פרטים והתחל לשלם למוניות מהפלאפון',
-      'MAIL': 'כתובת מייל',
-      'FULLNAME': 'שם מלא'
-
-  });
-
-  $translateProvider.preferredLanguage('he_IL');
+    $translateProvider.preferredLanguage('he_IL');
 });
 
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -46,23 +46,14 @@ appControllers.controller('SignupCtrl', function($scope, $ionicPopup, User) {
     $scope.user = {};
 
     $scope.signup = function () {
-        console.log($scope.user);
-        if (!$scope.user.username || !$scope.user.password || !$scope.user.mail || !$scope.user.fullname) {
-            $ionicPopup.alert({
-                title: 'שגיאה',
-                template: 'נא מלא את כל השדות'
-            });
-        }
-        else {
-            User.save($scope.user, function (responseData) {
-                if (!responseData.user) {
-                    $ionicPopup.alert({
-                        title: 'שגיאה',
-                        template: responseData.message
-                    });
-                }
-            });
-        }
+        User.save($scope.user, function (responseData) {
+            if (!responseData.user) {
+                $ionicPopup.alert({
+                    title: 'שגיאה',
+                    template: responseData.message
+                });
+            }
+        });
     }
 
 });
